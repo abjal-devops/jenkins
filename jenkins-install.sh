e5ce8460cb0daf23e577732e315d200bea88c013
abjal-devops <66158379+abjal-devops@users.noreply.github.com>
2020-09-09 19:49:09 +0530

    Update jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -5,6 +5,7 @@ tar -xzvf apache-maven-3.6.3-bin.tar.gz
 useradd -d /var/lib/jenkins -s /bin/bash jenkins 
 echo 'export PATH=$PATH:/opt/apache-maven-3.6.3/bin' >> /etc/profile
 echo 'export M2_HOME=/opt/apache-maven-3.6.3/' >> /etc/profile
+echo 'export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.252.b09-2.amzn2.0.1.x86_64/' >> /etc/profile
 source /etc/profile
 exec bash
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
