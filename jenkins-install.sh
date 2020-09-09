6e1efd0742053f1304fd31a03bc0807c8a808e55
abjal-devops <66158379+abjal-devops@users.noreply.github.com>
2020-09-09 18:53:00 +0530

    Create jenkins-install.sh

---
+++ jenkins-install.sh
@@ -0,0 +1,13 @@
+yum install java-1.8.0-openjdk-devel -y
+yum install git -y
+cd /opt && sudo wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xzvf apache-maven-3.6.3-bin.tar.gz
+useradd -d /var/lib/jenkins -s /bin/bash jenkins 
+echo 'export PATH=$PATH:/opt/apache-maven-3.6.3/bin' >> /etc/profile
+echo 'export M2_HOME=/opt/apache-maven-3.6.3/' >> /etc/profile
+source /etc/profile
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install jenkins -y
+systemctl start jenkins
+systemctl enable jenkins
