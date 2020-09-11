9c428a6f88968d36da2f57c1f1e69b6f56d63938
abjal-devops <66158379+abjal-devops@users.noreply.github.com>
2020-09-11 21:26:36 +0530

    Update jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -11,7 +11,7 @@ yum install jenkins -y
 git clone https://github.com/abjal-devops/jenkins-setup-files.git
 mkdir -p /var/lib/jenkins/plugins
 chown -R jenkins.jenkins /var/lib/jenkins/
-cp ./jenkins-setup-files/plugins/* /var/lib/jenkins/plugins && cp ./jenkins-setup-files/plugins1/* /var/lib/jenkins/plugins && cp ./jenkins-setup-files/config.xml /var/lib/jenkins/
+cp ./jenkins-setup-files/plugins/* /var/lib/jenkins/plugins && cp ./jenkins-setup-files/plugins1/* /var/lib/jenkins/plugins && cp ./jenkins-setup-files/*.xml /var/lib/jenkins/
 systemctl start jenkins
 systemctl enable jenkins
 sudo chown -R jenkins.jenkins /opt/apache-maven-3.6.2/
