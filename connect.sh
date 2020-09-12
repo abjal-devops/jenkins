bc3590dd0aff2f9c432128a123bf4c8d504ba642
abjal-devops <66158379+abjal-devops@users.noreply.github.com>
2020-09-12 13:46:54 +0530

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -5,3 +5,8 @@ ssh -o StrictHostKeyChecking=no -i /c/Users/abjal/Downloads/abs.pem ec2-user@$a 
 ssh -o StrictHostKeyChecking=no -i /c/Users/abjal/Downloads/abs.pem ec2-user@$a dos2unix jenkins-install.sh
 ssh -o StrictHostKeyChecking=no -i /c/Users/abjal/Downloads/abs.pem ec2-user@$a sudo sh jenkins-install.sh
 echo $a 
+b=`aws ec2 describe-instances     --filters "Name=tag:Name,Values=dev"    --query Reservations[*].Instances[*].PublicIpAddress     --output text`
+echo $b
+ssh -o StrictHostKeyChecking=no -i /c/Users/abjal/Downloads/abs.pem ec2-user@$a sudo yum install  -y docker
+ssh -o StrictHostKeyChecking=no -i /c/Users/abjal/Downloads/abs.pem ec2-user@$a sudo systemctl start docker
+ssh -o StrictHostKeyChecking=no -i /c/Users/abjal/Downloads/abs.pem ec2-user@$a sudo docker swarm init
